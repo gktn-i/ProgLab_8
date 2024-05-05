b680c1fcb4d4d2421928ea4d555293eb3b870773
goktan1 <goektan2004@icloud.com>
2024-05-05 13:35:18 +0200

    return all restaurants

--- Backend/restaurant.php
+++ Backend/restaurant.php
@@ -1,3 +1,20 @@
+<?php
+
 $mysqli = require 'database.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : '';
+
+
+function fetchAllRestaurants($mysqli)
+{
+    $sql = "SELECT id, name, address, phone FROM restaurants";
+    $result = $mysqli->query($sql);
+
+    $restaurants = [];
+    while ($row = $result->fetch_assoc()) {
+        $restaurants[] = $row;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($restaurants);
+}
