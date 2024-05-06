f419ccb12f892ac42ace30f4fc1e6a6d50618e6e
goktan1 <goektan2004@icloud.com>
2024-05-06 17:00:13 +0200

    Switch case for fetching restaurants

--- Backend/restaurant.php
+++ Backend/restaurant.php
@@ -5,6 +5,22 @@ $mysqli = require 'database.php';
 $action = isset($_GET['action']) ? $_GET['action'] : '';
 
 
+switch ($action) {
+    case 'all':
+        fetchAllRestaurants($mysqli);
+        break;
+    case 'single':
+        if (isset($_GET['id'])) {
+            $restaurantId = intval($_GET['id']); //schutz vor sql injection 
+            fetchSingleRestaurant($mysqli, $restaurantId);
+        } else {
+            echo json_encode(['error' => 'Keine Restaurant-ID angegeben']);
+        }
+        break;
+    default:
+        echo json_encode(['error' => 'Ungültige Aktion']);
+}
+
 function fetchAllRestaurants($mysqli)
 {
     $sql = "SELECT id, name, address, phone FROM restaurants";
