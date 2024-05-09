65b905443d75dc5bcf5012853b5aea9cec3349a7
Sine <ssivakum@stud.fra-uas.de>
2024-05-09 20:21:58 +0200

    changing speed

--- index.php
+++ index.php
@@ -143,99 +143,108 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function () {
-            var myChart;
+    var myChart;
+    var requestData = null;
+    var selectedRadio = 'firstRadio'; // Standardmäßig auf 'Best Selling Product' gesetzt
 
-            function showListData(data) {
-                var listItems = "";
-                $.each(data, function (index, item) {
-                    listItems += '<li class="list-group-item">' +
-                        '<label>Name: ' + item.Name + '</label><br>' +
-                        '<span>Size: ' + item.Size + '</span><br>' +
-                        '<span>Order Count: ' + item.orderCount + '</span>' +
-                        '</li>';
-                });
-                $('#dataList').html(listItems);
-            }
-
-            function createBarChart(labels, orderCounts) {
-                var ctx = document.getElementById('myChart').getContext('2d');
-
-
-                if (myChart) {
-                    myChart.destroy();
-                }
-
-                myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            label: 'Order Count',
-                            data: orderCounts,
-                            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                            borderColor: 'rgba(54, 162, 235, 1)',
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
-                    }
-                });
-            }
-
-            function fetchDataAndDisplay(displayType) {
-                $.ajax({
-                    type: "GET",
-                    url: "Backend/get_product_count_data.php",
-                    dataType: "json",
-                    success: function (data) {
-                        if ((displayType === 'list' && $('input[name=listGroupRadio]:checked').attr('id') === 'firstRadio') ||
-                            (displayType === 'chart' && $('input[name=listGroupRadio]:checked').attr('id') === 'firstRadio')) {
-                            if (displayType === 'list') {
-                                showListData(data);
-                                $('#dataList').show();
-                                $('#myChart').hide();
-                            } else if (displayType === 'chart') {
-                                var labels = [];
-                                var orderCounts = [];
-                                $.each(data, function (index, item) {
-                                    labels.push(item.Name);
-                                    orderCounts.push(item.orderCount);
-                                });
-                                createBarChart(labels, orderCounts);
-                                $('#dataList').hide();
-                                $('#myChart').show();
-                            }
-                        } else {
-                            $('#dataList').hide();
-                            $('#myChart').hide();
-                        }
-                    },
-                    error: function (xhr, status, error) {
-                        console.error(xhr.responseText);
-                    }
-                });
-            }
-
-            var defaultTheme = $('#filter_options1').val();
-            fetchDataAndDisplay(defaultTheme);
-
-           
-            $('input[name=listGroupRadio]').change(function () {
-                fetchDataAndDisplay($('#filter_options1').val()); 
-            });
-
-            $('#filter_options1').change(function () {
-                var selectedTheme = $(this).val();
-                fetchDataAndDisplay(selectedTheme);
-            });
+    function showListData(data) {
+        var listItems = "";
+        $.each(data, function (index, item) {
+            listItems += '<li class="list-group-item">' +
+                '<label>Name: ' + item.Name + '</label><br>' +
+                '<span>Size: ' + item.Size + '</span><br>' +
+                '<span>Order Count: ' + item.orderCount + '</span>' +
+                '</li>';
         });
+        $('#dataList').html(listItems);
+        $('#dataList').show();
+        $('#myChart').hide();
+    }
+
+    function createBarChart(labels, orderCounts) {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        if (myChart) {
+            myChart.destroy();
+        }
+
+        myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Order Count',
+                    data: orderCounts,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+        $('#myChart').show();
+        $('#dataList').hide();
+    }
+
+    function fetchData() {
+        $.ajax({
+            type: "GET",
+            url: "Backend/get_product_count_data.php",
+            dataType: "json",
+            success: function (data) {
+                requestData = data;
+                updateDisplay($('#filter_options1').val(), selectedRadio);
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+            }
+        });
+    }
+
+    function updateDisplay(displayType, selectedRadio) {
+        if (requestData === null) {
+            return; 
+        }
+
+        if (displayType === 'list' && selectedRadio === 'firstRadio') {
+            showListData(requestData);
+        } else if (displayType === 'chart' && selectedRadio === 'firstRadio') {
+            var labels = [];
+            var orderCounts = [];
+            $.each(requestData, function (index, item) {
+                labels.push(item.Name);
+                orderCounts.push(item.orderCount);
+            });
+            createBarChart(labels, orderCounts);
+        } else {
+            $('#dataList').hide();
+            $('#myChart').hide();
+        }
+    }
+
+    var defaultTheme = $('#filter_options1').val();
+    fetchData();
+    updateDisplay(defaultTheme, selectedRadio);
+
+
+    $('#filter_options1').change(function () {
+        var selectedTheme = $(this).val();
+        updateDisplay(selectedTheme, selectedRadio);
+    });
+
+    $('input[name=listGroupRadio]').change(function () {
+        selectedRadio = $(this).attr('id');
+        updateDisplay($('#filter_options1').val(), selectedRadio);
+    });
+});
     </script>
 </body>
 
