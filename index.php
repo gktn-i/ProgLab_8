ddb271f6dfcf10b22cba48957ae30aba4e2fba41
Sine <ssivakum@stud.fra-uas.de>
2024-05-09 20:50:49 +0200

    FE minimal changes

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             margin-right: 20px;
         }
-    
+
 
         .right-section {
             flex-basis: 70%;
@@ -57,7 +57,7 @@
             margin-top: 5%px;
         }
 
-      
+
         .list-group-item label {
             font-weight: bold;
         }
@@ -71,10 +71,11 @@
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             margin-right: 20px;
         }
+
         .list-group-item:hover {
-            background-color: #e9e9e9; 
+            background-color: #e9e9e9;
         }
-      
+
 
         .form-group2 {
             margin-bottom: 20px;
@@ -85,14 +86,15 @@
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             margin-right: 20px;
         }
+
         select {
             width: 100%;
             padding: 10px;
             border: 1px solid #ccc;
             border-radius: 5px;
             background-color: #f9f9f9;
-            font-size: 16px; 
-            appearance: none; 
+            font-size: 16px;
+            appearance: none;
         }
     </style>
 </head>
@@ -156,110 +158,112 @@
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <script>
+
         $(document).ready(function () {
-    var myChart;
-    var requestData = null;
-    var selectedRadio = 'firstRadio'; 
+            var myChart;
+            var requestData = null;
+            var selectedRadio = 'firstRadio';
 
-    function showListData(data) {
-        var listItems = "";
-        $.each(data, function (index, item) {
-            listItems += '<li class="list-group-item">' +
-                '<label>Gericht: ' + item.Name + '</label><br>' +
-                '<span>Size: ' + item.Size + '</span><br>' +
-                '<span>Order Count: ' + item.orderCount + '</span>' +
-                '</li>';
-        });
-        $('#dataList').html(listItems);
-        $('#dataList').show();
-        $('#myChart').hide();
-    }
+            function showListData(data) {
+                var listItems = "";
+                $.each(data, function (index, item) {
+                    listItems += '<li class="list-group-item">' +
+                        '<label>Gericht: ' + item.Name + '</label><br>' +
+                        '<span>Size: ' + item.Size + '</span><br>' +
+                        '<span>Order Count: ' + item.orderCount + '</span>' +
+                        '</li>';
+                });
+                $('#dataList').html(listItems);
+                $('#dataList').show();
+                $('#myChart').hide();
+            }
 
-    function createBarChart(labels, orderCounts) {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        if (myChart) {
-            myChart.destroy();
-        }
+            function createBarChart(labels, orderCounts) {
+                var ctx = document.getElementById('myChart').getContext('2d');
+                if (myChart) {
+                    myChart.destroy();
+                }
 
-        myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Order Count',
-                    data: orderCounts,
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
+                myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Order Count',
+                            data: orderCounts,
+                            backgroundColor: 'rgba(60, 81, 49, 0.2)',
+                            borderColor: 'rgb(60, 81, 49, 1)',
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
                         }
-                    }]
+                    }
+                });
+
+                $('#myChart').show();
+                $('#dataList').hide();
+            }
+
+            function fetchData() {
+                $.ajax({
+                    type: "GET",
+                    url: "Backend/get_product_count_data.php",
+                    dataType: "json",
+                    success: function (data) {
+                        requestData = data;
+                        updateDisplay($('#filter_options1').val(), selectedRadio);
+                    },
+                    error: function (xhr, status, error) {
+                        console.error(xhr.responseText);
+                    }
+                });
+            }
+
+            function updateDisplay(displayType, selectedRadio) {
+                if (requestData === null) {
+                    return;
+                }
+
+                if (displayType === 'list' && selectedRadio === 'firstRadio') {
+                    showListData(requestData);
+                } else if (displayType === 'chart' && selectedRadio === 'firstRadio') {
+                    var labels = [];
+                    var orderCounts = [];
+                    $.each(requestData, function (index, item) {
+                        labels.push(item.Name);
+                        orderCounts.push(item.orderCount);
+                    });
+                    createBarChart(labels, orderCounts);
+                } else {
+                    $('#dataList').hide();
+                    $('#myChart').hide();
                 }
             }
-        });
 
-        $('#myChart').show();
-        $('#dataList').hide();
-    }
+            var defaultTheme = $('#filter_options1').val();
+            fetchData();
+            updateDisplay(defaultTheme, selectedRadio);
 
-    function fetchData() {
-        $.ajax({
-            type: "GET",
-            url: "Backend/get_product_count_data.php",
-            dataType: "json",
-            success: function (data) {
-                requestData = data;
-                updateDisplay($('#filter_options1').val(), selectedRadio);
-            },
-            error: function (xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
-    }
 
-    function updateDisplay(displayType, selectedRadio) {
-        if (requestData === null) {
-            return; 
-        }
-
-        if (displayType === 'list' && selectedRadio === 'firstRadio') {
-            showListData(requestData);
-        } else if (displayType === 'chart' && selectedRadio === 'firstRadio') {
-            var labels = [];
-            var orderCounts = [];
-            $.each(requestData, function (index, item) {
-                labels.push(item.Name);
-                orderCounts.push(item.orderCount);
+            $('#filter_options1').change(function () {
+                var selectedTheme = $(this).val();
+                updateDisplay(selectedTheme, selectedRadio);
             });
-            createBarChart(labels, orderCounts);
-        } else {
-            $('#dataList').hide();
-            $('#myChart').hide();
-        }
-    }
 
-    var defaultTheme = $('#filter_options1').val();
-    fetchData();
-    updateDisplay(defaultTheme, selectedRadio);
-
-
-    $('#filter_options1').change(function () {
-        var selectedTheme = $(this).val();
-        updateDisplay(selectedTheme, selectedRadio);
-    });
-
-    $('input[name=listGroupRadio]').change(function () {
-        selectedRadio = $(this).attr('id');
-        updateDisplay($('#filter_options1').val(), selectedRadio);
-    });
-});
+            $('input[name=listGroupRadio]').change(function () {
+                selectedRadio = $(this).attr('id');
+                updateDisplay($('#filter_options1').val(), selectedRadio);
+            });
+        });
     </script>
 </body>
 
