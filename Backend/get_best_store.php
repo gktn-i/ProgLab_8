36bfeaecd28a8290255c453c0d94e4e523e94417
goktan1 <goektan2004@icloud.com>
2024-05-20 17:24:00 +0200

    update

--- Backend/get_best_store.php
+++ Backend/get_best_store.php
@@ -2,20 +2,22 @@
 require 'database.php';
 
 $query = "
-    SELECT stores.store_name, SUM(orderItems.quantity * products.price) as turnover
-    FROM orders
+    SELECT stores.store_id, stores.store_name, SUM(orderItems.quantity * products.price) as turnover
+    FROM stores
+    JOIN orders ON stores.store_id = orders.store_id
     JOIN orderItems ON orders.order_id = orderItems.order_id
     JOIN products ON orderItems.product_id = products.product_id
-    JOIN stores ON orders.store_id = stores.store_id
-    GROUP BY stores.store_id
+    GROUP BY stores.store_id, stores.store_name
     ORDER BY turnover DESC
     LIMIT 1
 ";
 
-$stmt = $pdo->prepare($query);
-$stmt->execute();
-$best_store = $stmt->fetch(PDO::FETCH_ASSOC);
+$result = mysqli_query($conn, $query);
+
+$bestStore = mysqli_fetch_assoc($result);
 
 header('Content-Type: application/json');
-echo json_encode($best_store);
+echo json_encode($bestStore);
+
+mysqli_close($conn);
 ?>
