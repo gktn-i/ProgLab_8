73e6807fd2f648650fbd1887a73114a3373c4724
goktan1 <goektan2004@icloud.com>
2024-05-20 18:07:30 +0200

    added ajax chart function

--- Backend/script.js
+++ Backend/script.js
@@ -33,17 +33,6 @@ function fetchDataCustomerOrders() {
     });
 }
 
-function fetchBestStore() {
-    fetch('get_best_store.php')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('best-store-name').innerText = data.store_name;
-            document.getElementById('best-store-turnover').innerText = `Turnover: $${data.turnover.toFixed(2)}`;
-            renderTurnoverChart(data);
-        })
-        .catch(error => console.error('Error fetching best store data:', error));
-}
-
 function showListData(data) {
     var listItems = "";
     $.each(data, function (index, item) {
@@ -129,90 +118,112 @@ function createCustomerBarChart(labels, data) {
                 }]
             }
         }
-    });
-
-    $('#myChart').show();
-    $('#dataList').hide();
-}
-
-function renderTurnoverChart(data) {
-    var ctx = document.getElementById('turnoverChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: [data.store_name],
-            datasets: [{
-                label: 'Turnover',
-                data: [data.turnover],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+        });
+        
+        $('#myChart').show();
+        $('#dataList').hide();
+        }
+        
+        $(document).ready(function () {
+        var selectedRadio = 'firstRadio';
+        
+        function updateDisplay(displayType, selectedRadio) {
+            if (selectedRadio === 'firstRadio') {
+                if (displayType === 'list') {
+                    if (requestDataProducts) {
+                        showListData(requestDataProducts);
+                    }
+                } else if (displayType === 'chart') {
+                    if (requestDataProducts) {
+                        var labels = [];
+                        var orderCounts = [];
+                        $.each(requestDataProducts, function (index, item) {
+                            labels.push(item.Name);
+                            orderCounts.push(item.orderCount);
+                        });
+                        createBarChart(labels, orderCounts);
+                    }
                 }
+            } else if (selectedRadio === 'thirdRadio') {
+                if (displayType === 'list') {
+                    if (requestDataCustomers) {
+                        showCustomerOrders(requestDataCustomers);
+                    }
+                } else if (displayType === 'chart') {
+                    if (requestDataCustomers) {
+                        var labels = [];
+                        var customerCounts = [];
+                        $.each(requestDataCustomers, function (index, item) {
+                            labels.push("Customer " + item.customerID);
+                            customerCounts.push(item.customerCount);
+                        });
+                        createCustomerBarChart(labels, customerCounts);
+                    }
+                }
+            } else if (selectedRadio === 'secondRadio') {
+                fetchBestStore();
             }
         }
-    });
-}
-
-$(document).ready(function () {
-    var selectedRadio = 'firstRadio';
-
-    function updateDisplay(displayType, selectedRadio) {
-        $('#best-store-card').addClass('hidden'); // Hide the best store card by default
-        if (selectedRadio === 'firstRadio') {
-            if (displayType === 'list') {
-                if (requestDataProducts) {
-                    showListData(requestDataProducts);
+        
+        function fetchBestStore() {
+            $.ajax({
+                type: "GET",
+                url: "Backend/get_best_store.php",
+                dataType: "json",
+                success: function (data) {
+                    var labels = [data.store_name];
+                    var turnoverData = [data.turnover];
+                    createTurnoverChart(labels, turnoverData);
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
                 }
-            } else if (displayType === 'chart') {
-                if (requestDataProducts) {
-                    var labels = [];
-                    var orderCounts = [];
-                    $.each(requestDataProducts, function (index, item) {
-                        labels.push(item.Name);
-                        orderCounts.push(item.orderCount);
-                    });
-                    createBarChart(labels, orderCounts);
-                }
-            }
-        } else if (selectedRadio === 'thirdRadio') {
-            if (displayType === 'list') {
-                if (requestDataCustomers) {
-                    showCustomerOrders(requestDataCustomers);
-                }
-            } else if (displayType === 'chart') {
-                if (requestDataCustomers) {
-                    var labels = [];
-                    var customerCounts = [];
-                    $.each(requestDataCustomers, function (index, item) {
-                        labels.push("Customer " + item.customerID);
-                        customerCounts.push(item.customerCount);
-                    });
-                    createCustomerBarChart(labels, customerCounts);
-                }
-            }
-        } else if (selectedRadio === 'secondRadio') {
-            $('#best-store-card').removeClass('hidden');
-            fetchBestStore();
+            });
         }
-    }
-
-    $('#filter_options1').change(function () {
-        var selectedTheme = $(this).val();
-        updateDisplay(selectedTheme, selectedRadio);
-    });
-
-    $('input[name=listGroupRadio]').change(function () {
-        selectedRadio = $(this).attr('id');
-        updateDisplay($('#filter_options1').val(), selectedRadio);
-    });
-
-    // Initial fetch
-    fetchData();
-    fetchDataCustomerOrders();
-});
+        
+        function createTurnoverChart(labels, data) {
+            var ctx = document.getElementById('myChart').getContext('2d');
+            if (myChart) {
+                myChart.destroy();
+            }
+        
+            myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Turnover',
+                        data: data,
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        
+            $('#myChart').show();
+            $('#dataList').hide();
+        }
+        
+        $('#filter_options1').change(function () {
+            var selectedTheme = $(this).val();
+            updateDisplay(selectedTheme, selectedRadio);
+        });
+        
+        $('input[name=listGroupRadio]').change(function () {
+            selectedRadio = $(this).attr('id');
+            updateDisplay($('#filter_options1').val(), selectedRadio);
+        });
+        
+        // Initial fetch
+        fetchData();
+        fetchDataCustomerOrders();
+        });
+        
