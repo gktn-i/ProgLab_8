fafbcad9aecb94aef13fafed391986ab5d812bfc
Sine <ssivakum@stud.fra-uas.de>
2024-05-20 21:41:08 +0200

    Revert "update"
    This reverts commit 36bfeaecd28a8290255c453c0d94e4e523e94417.

--- Backend/get_best_store.php
+++ Backend/get_best_store.php
@@ -2,22 +2,20 @@
 require 'database.php';
 
 $query = "
-    SELECT stores.store_id, stores.store_name, SUM(orderItems.quantity * products.price) as turnover
-    FROM stores
-    JOIN orders ON stores.store_id = orders.store_id
+    SELECT stores.store_name, SUM(orderItems.quantity * products.price) as turnover
+    FROM orders
     JOIN orderItems ON orders.order_id = orderItems.order_id
     JOIN products ON orderItems.product_id = products.product_id
-    GROUP BY stores.store_id, stores.store_name
+    JOIN stores ON orders.store_id = stores.store_id
+    GROUP BY stores.store_id
     ORDER BY turnover DESC
     LIMIT 1
 ";
 
-$result = mysqli_query($conn, $query);
-
-$bestStore = mysqli_fetch_assoc($result);
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+$best_store = $stmt->fetch(PDO::FETCH_ASSOC);
 
 header('Content-Type: application/json');
-echo json_encode($bestStore);
-
-mysqli_close($conn);
+echo json_encode($best_store);
 ?>
