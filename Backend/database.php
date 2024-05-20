336af24fad50137005cc26e474f1e266e5bece9d
Sine <ssivakum@stud.fra-uas.de>
2024-05-20 19:10:12 +0200

    Update database.php

--- Backend/database.php
+++ Backend/database.php
@@ -10,15 +10,15 @@ $username = "root";
 $password = "";
 
 $mysqli = new mysqli(
-    hostname: $host,
-    username: $username,
-    password: $password,
-    database: $dbname,
+    $host,
+    $username,
+    $password,
+    $dbname
 );
 
 if ($mysqli->connect_errno) {
-    die("Connection error". $mysqli->connect_error);
-
+    die("Connection error: " . $mysqli->connect_error);
 }
 
 return $mysqli;
+?>
