91edb2f2d3aecaa1b0aa9a9ed7ddc49a04f6357b
goktan1 <goektan2004@icloud.com>
2024-05-23 19:43:02 +0200

    Added particles.js script, modified CSS styles, and rearranged HTML structure.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    
+
     <title>Dashboard</title>
     <script src="Backend/script.js"></script>
     <style>
@@ -57,6 +57,8 @@
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             text-align: center;
             animation: fadeIn 1s ease-in;
+            border: 1px solid;
+            border-color:#666;
         }
 
         .stat-content {
@@ -92,7 +94,7 @@
             max-width: 1200px;
             margin: 50px auto;
             margin-bottom: 10%;
-            
+
         }
 
         .left-section {
@@ -102,7 +104,9 @@
             border-radius: 10px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             margin-right: 20px;
-            
+            border: 1px solid;
+            border-color:#666;
+
         }
 
 
@@ -112,6 +116,8 @@
             background-color: #fff;
             border-radius: 10px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            border: 1px solid;
+            border-color:#666;
         }
 
         h1,
@@ -154,7 +160,7 @@
         .list-group-item:hover {
             background-color: #e9e9e9;
             transition: opacity 0.3s;
-           
+
         }
 
 
@@ -168,7 +174,7 @@
             margin-right: 20px;
         }
 
-      
+
         select {
             width: 100%;
             padding: 10px;
@@ -179,131 +185,150 @@
             appearance: none;
         }
 
-        .filter_options{
+        .filter_options {
             margin-bottom: 10px;
         }
 
         #map {
-        height: 400px;
-        margin: 20px auto;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-        border-color: #121212;
-    }
+            height: 400px;
+            margin: 20px auto;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            border-color: #121212;
+        }
 
+        #particles-js {
+            position: fixed;
+            /* Festposition, unabhängig von Scrollen */
+            top: 0;
+            /* Oberer Rand auf 0 setzen */
+            left: 0;
+            /* Linker Rand auf 0 setzen */
+            width: 100vw;
+            /* 100% der viewport Breite */
+            height: 100vh;
+            /* 100% der viewport Höhe */
+            z-index: -1;
+            /* Hintergrundebene */
+        }
     </style>
 </head>
 
 
 <body>
+
+<div id="particles-js" class="hintergrund"></div>
+
+
+
     <?php include 'Navbar.php'; ?>
     <?php include 'Footer.php'; ?>
 
     <div class="statistics">
-    <div class="stat-box">
-        <h2><i class='bx bxs-user-detail'></i> <span id="totalOrders">0</span></h2>
-        <p>Total Orders</p>
-    </div>
-    <div class="stat-box">
-        <h2><i class='bx bxs-dollar-circle'></i> <span id="totalRevenue">$0.00</span></h2>
-        <p>Total Revenue</p>
-    </div>
-    <div class="stat-box">
-        <h2><i class='bx bxs-group'></i> <span id="totalCustomers">0</span></h2>
-        <p>Total Customers</p>
-    </div>
-    <div class="stat-box">
-        <h2><i class='bx bxs-box'></i> <span id="totalProducts">0</span></h2>
-        <p>Total Products</p>
-    </div>
-</div>
-
-<div id="map"></div>
-
-<div class="container">
-    <div class="left-section">
-        <?php if (isset($error_message)): ?>
-            <p style="color: red;"><?php echo $error_message; ?></p>
-        <?php endif; ?>
-        <div class="form-group">
-            <label for="filter_options">Select Time Range</label>
-            <select id="filter_options" name="filter_options">
-                <option value="all">ALL</option>
-                <option value="week">Week</option>
-                <option value="month">Month</option>
-                <option value="year">Year</option>
-            </select>
+        <div class="stat-box">
+            <h2><i class='bx bxs-user-detail'></i> <span id="totalOrders">0</span></h2>
+            <p>Total Orders</p>
         </div>
-        <div class="form-group2">
-            <label for="filter_options">Theme</label>
-            <select id="filter_options1" name="filter_options1">
-                <option value="list">List</option>
-                <option value="chart">Chart</option>
-            </select>
+        <div class="stat-box">
+            <h2><i class='bx bxs-dollar-circle'></i> <span id="totalRevenue">$0.00</span></h2>
+            <p>Total Revenue</p>
         </div>
-        <ul class="list-group">
-            <li class="list-group-item">
-                <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio"
-                    checked>
-                <label class="form-check-label" for="firstRadio">Best seller product</label>
-            </li>
-            <li class="list-group-item">
-                <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="secondRadio">
-                <label class="form-check-label" for="secondRadio">Turnover</label>
-            </li>
-            <li class="list-group-item">
-                <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="thirdRadio">
-                <label class="form-check-label" for="thirdRadio">Customer Count</label>
-            </li>
-            <li class="list-group-item">
-                <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="fourthRadio">
-                <label class="form-check-label" for="fourthRadio">Best ?</label>
-            </li>
-            <li class="list-group-item">
-                <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="fifthRadio">
-                <label class="form-check-label" for="fifthRadio">Order Count</label>
-            </li>
-        </ul>
-    </div>
-    <div class="right-section">
-        <h1>Statistics</h1>
-        <div id="chartContainer">
-            <canvas id="myChart"></canvas>
+        <div class="stat-box">
+            <h2><i class='bx bxs-group'></i> <span id="totalCustomers">0</span></h2>
+            <p>Total Customers</p>
         </div>
-        <ul class="list-group" id="dataList">
-            <!-- Placeholder for list data -->
-        </ul>
+        <div class="stat-box">
+            <h2><i class='bx bxs-box'></i> <span id="totalProducts">0</span></h2>
+            <p>Total Products</p>
+        </div>
     </div>
-</div>
-<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-            <script>
-                // AJAX request to get location data from map.php
-                fetch('/Backend/map.php')
-                    .then(response => response.json())
-                    .then(locations => {
-                        var map = L.map('map');
 
-                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        }).addTo(map);
+    <div id="map"></div>
 
-                        var markers = locations.map(function(location) {
-                            var marker = L.marker([location.latitude, location.longitude]);
-                            
-                            marker.bindPopup(
-                                '<b>Store ID:</b> ' + location.storeID + '<br>' +
-                                '<b>City:</b> ' + location.city + '<br>' +
-                                '<b>Zip Code:</b> ' + location.zipcode + '<br>' +
-                                '<b>State:</b> ' + location.state + ' (' + location.state_abbr + ')'
-                            );
-                            return marker;
-                        });
+    <div class="container">
+        <div class="left-section">
+            <?php if (isset($error_message)) : ?>
+                <p style="color: red;"><?php echo $error_message; ?></p>
+            <?php endif; ?>
+            <div class="form-group">
+                <label for="filter_options">Select Time Range</label>
+                <select id="filter_options" name="filter_options">
+                    <option value="all">ALL</option>
+                    <option value="week">Week</option>
+                    <option value="month">Month</option>
+                    <option value="year">Year</option>
+                </select>
+            </div>
+            <div class="form-group2">
+                <label for="filter_options">Theme</label>
+                <select id="filter_options1" name="filter_options1">
+                    <option value="list">List</option>
+                    <option value="chart">Chart</option>
+                </select>
+            </div>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="firstRadio" checked>
+                    <label class="form-check-label" for="firstRadio">Best seller product</label>
+                </li>
+                <li class="list-group-item">
+                    <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="secondRadio">
+                    <label class="form-check-label" for="secondRadio">Turnover</label>
+                </li>
+                <li class="list-group-item">
+                    <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="thirdRadio">
+                    <label class="form-check-label" for="thirdRadio">Customer Count</label>
+                </li>
+                <li class="list-group-item">
+                    <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="fourthRadio">
+                    <label class="form-check-label" for="fourthRadio">Best ?</label>
+                </li>
+                <li class="list-group-item">
+                    <input class="form-check-input me-1" type="radio" name="listGroupRadio" value="" id="fifthRadio">
+                    <label class="form-check-label" for="fifthRadio">Order Count</label>
+                </li>
+            </ul>
+        </div>
+        <div class="right-section">
+            <h1>Statistics</h1>
+            <div id="chartContainer">
+                <canvas id="myChart"></canvas>
+            </div>
+            <ul class="list-group" id="dataList">
+                <!-- Placeholder for list data -->
+            </ul>
+        </div>
+    </div>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script>
+        // AJAX request to get location data from map.php
+        fetch('/Backend/map.php')
+            .then(response => response.json())
+            .then(locations => {
+                var map = L.map('map');
 
-                        var featureGroup = L.featureGroup(markers).addTo(map);
-                        map.fitBounds(featureGroup.getBounds());
-                    })
-                    .catch(error => console.error('Error fetching data:', error));
-            </script>
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
 
+                var markers = locations.map(function(location) {
+                    var marker = L.marker([location.latitude, location.longitude]);
+
+                    marker.bindPopup(
+                        '<b>Store ID:</b> ' + location.storeID + '<br>' +
+                        '<b>City:</b> ' + location.city + '<br>' +
+                        '<b>Zip Code:</b> ' + location.zipcode + '<br>' +
+                        '<b>State:</b> ' + location.state + ' (' + location.state_abbr + ')'
+                    );
+                    return marker;
+                });
+
+                var featureGroup = L.featureGroup(markers).addTo(map);
+                map.fitBounds(featureGroup.getBounds());
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    </script>
+    <script type="text/javascript " src="particles.js "></script>
+    <script src="app.js "></script>
 
 </body>
 
