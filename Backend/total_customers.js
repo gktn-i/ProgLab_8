89c5661d7bef1f2a24ea8fb8de3dc56b643115c8
Sine <ssivakum@stud.fra-uas.de>
2024-06-12 16:56:04 +0200

    Update total_customers.js

--- Backend/total_customers.js
+++ Backend/total_customers.js
@@ -28,7 +28,7 @@ function createChart(data) {
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)'
+                    'rgba(255, 206, 86, 2)'
                 ],
                 borderWidth: 1
             }]
